b26ba57c991cc9d0d2789a2d4ebd02cf5ce7dad2
mubashir-mehran <mmehran056@gmail.com>
2024-02-24 17:45:08 +0500

    Completed User Table

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
-import DetailForm from "./pages/DetailForm";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyArQvT9y-z_D7_Xc7azefrSzh3SGWXaXWw",
-  authDomain: "test-tezeract.firebaseapp.com",
-  projectId: "test-tezeract",
-  storageBucket: "test-tezeract.appspot.com",
-  messagingSenderId: "341103227495",
-  appId: "1:341103227495:web:f475c8407ef36fbe9fdac2",
-  measurementId: "G-MLEEDTW5WK",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+import UserTable from "./pages/UserTableComponent";
 
 function App() {
-  return <DetailForm />;
+  return <UserTable />;
 }
 export default App;
--- src/pages/UserTableComponent.js
+++ src/pages/UserTableComponent.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 import styles from "../styles/usertable.module.css";
 import { Switch } from "antd";
-import { db, storage } from "../App";
-import { collection, getDocs } from "firebase/firestore";
-import { ref, list, listAll, getDownloadURL } from "firebase/storage";
+import { DeleteOutlined } from "@ant-design/icons";
+import { EditOutlined } from "@ant-design/icons";
+
 import { Spin } from "antd";
 
 const UserTableComponent = () => {
-  const [data, setData] = useState([]);
+  const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const itemsPerPage = 10;
-  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const totalPages = Math.ceil(userData.length / itemsPerPage);
 
   const [currentPage, setCurrentPage] = useState(1);
 
-  const [imageURLs, setImageURLs] = useState([]);
   const [activeUsers, setActiveUsers] = useState(
-    data.map((user) => user.active)
+    userData.map((user) => user.active)
   );
 
   const toggleActive = (index) => {
@@ -35,130 +35,136 @@ const UserTableComponent = () => {
   };
 
   const startIndex = (currentPage - 1) * itemsPerPage + 1;
-  const endIndex = Math.min(startIndex + itemsPerPage - 1, data.length);
-  const visibleData = data.slice(startIndex - 1, endIndex);
+  const endIndex = Math.min(startIndex + itemsPerPage - 1, userData.length);
+  const visibleData = userData.slice(startIndex - 1, endIndex);
 
-  const totalEntries = data.length;
+  const totalEntries = userData.length;
 
-  const fetchImages = async () => {
-    const storageRef = ref(storage, "images/");
-    const result = await listAll(storageRef);
-
-    const urlPromises = result.items.map((imageRef) =>
-      getDownloadURL(imageRef)
-    );
-
-    try {
-      const urls = await Promise.all(urlPromises);
-      setImageURLs(urls);
-    } catch (error) {
-      console.error("Error retrieving image URLs", error);
-    }
-  };
+  const filteredData = userData.filter((user) =>
+    user.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   const fetchPost = async () => {
     setLoading(true);
     try {
-      await getDocs(collection(db, "users")).then((querySnapshot) => {
-        const newData = querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
+      const newData = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
 
-        setData(newData);
-      });
+      setUserData(newData);
     } catch (error) {
       console.error("Error fetching data", error);
     } finally {
-      setLoading(false); // Set loading to false whether successful or not
+      setLoading(false);
     }
   };
   useEffect(() => {
     fetchPost();
-    fetchImages();
   }, []);
-
   return (
-    <div className={styles.user_table_container}>
-      <h2>User Table</h2>
-      <div className={styles.spinner}>
-        <Spin spinning={loading} tip="Loading..." />
-      </div>
-      <p>
-        Showing <span className={styles.total_entries}>{totalEntries}</span>{" "}
-        entries
-      </p>
-      {!loading && (
-        <table className={styles.user_table}>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>EMAIL</th>
-              <th>NAME</th>
-              <th>PHONE NO</th>
-              <th>INTERVIEW TIMINGS</th>
-              <th>ROLE</th>
-              <th>ACTIVE</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((user, index) => (
-              <tr key={user.id} className={styles.table_data}>
-                <td>{index + 1}</td>
-                <td>{user?.email}</td>
-                <td className={styles.image_profile}>
-                  {
-                    <img
-                      src={user.profileURL}
-                      className={styles.profile_img}
-                      width="20px"
-                      height="20px"
-                    />
-                  }
-                  {user?.username}
-                </td>
-                <td>{user?.phone}</td>
-                <td>{user?.time}</td>
-                <td>{user?.role}</td>
-                <td className={styles.add_button}>
-                  <Switch
-                    checked={activeUsers[index]}
-                    onChange={() => toggleActive(index)}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-      <div className={styles.pagination}>
-        <div className={styles.entries}>
-          <p>{`Showing ${startIndex} to ${endIndex} of ${data.length} entries`}</p>
+    <div className={styles.table_body}>
+      <div className={styles.user_table_container}>
+        <h2>User Table</h2>
+        <div className={styles.spinner}>
+          <Spin spinning={loading} tip="Loading..." />
         </div>
-        <div className={styles.pagination_num}>
-          <button onClick={handlePrevPage} disabled={currentPage === 1}>
-            Previous
-          </button>
-          <span>
-            {Array.from({ length: totalPages }, (_, index) => (
-              <span
-                key={index + 1}
-                onClick={() => setCurrentPage(index + 1)}
-                style={{
-                  color: currentPage === index + 1 ? "purple" : "black",
-                  cursor: "pointer",
-                }}
-              >
-                {index + 1}
-              </span>
-            ))}
-          </span>
-          <button
-            onClick={handleNextPage}
-            disabled={currentPage === totalPages}
-          >
-            Next
-          </button>
+        <input
+          className={styles.search_bar}
+          type="text"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <p>
+          Showing <span className={styles.total_entries}>{totalEntries}</span>{" "}
+          entries
+        </p>
+        {!loading && (
+          <table className={`${styles.user_table} ${styles.box_shadow}`}>
+            <thead>
+              <tr>
+                <th>
+                  <input type="checkbox" id="checkbox1" />
+                </th>
+                <th>User ID</th>
+                <th>Post ID</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Website</th>
+                <th>Company</th>
+                <th></th>
+                <th></th>
+                <th className={styles.column}>...</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userData.map((user, index) => {
+                return (
+                  <tr key={user.id} className={styles.table_data}>
+                    <td>
+                      <input type="checkbox" id="checkbox1" />
+                    </td>
+                    <td>{user?.id}</td>
+                    <td>{user?.id}</td>
+                    <td>{user?.name}</td>
+                    <td>{user?.username}</td>
+                    <td>{user?.name}</td>
+                    <td>{user?.username}</td>
+                    <td>{user?.email}</td>
+                    <td>{user?.address?.street}</td>
+                    <td>{user?.phone}</td>
+                    <td>{user?.website}</td>
+                    <td>{user?.company?.name}</td>
+
+                    <td className={styles.add_button}>
+                      <Switch
+                        checked={activeUsers[index]}
+                        onChange={() => toggleActive(index)}
+                      />
+                    </td>
+                    <td>{<EditOutlined />}</td>
+                    <td> {<DeleteOutlined />}</td>
+                    <td>...</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+        <div className={styles.pagination}>
+          <div className={styles.entries}>
+            <p>{`Showing ${startIndex} to ${endIndex} of ${userData.length} entries`}</p>
+          </div>
+          <div className={styles.pagination_num}>
+            <button onClick={handlePrevPage} disabled={currentPage === 1}>
+              Previous
+            </button>
+            <span>
+              {Array.from({ length: totalPages }, (_, index) => (
+                <span
+                  key={index + 1}
+                  onClick={() => setCurrentPage(index + 1)}
+                  style={{
+                    color: currentPage === index + 1 ? "purple" : "black",
+                    cursor: "pointer",
+                  }}
+                >
+                  {index + 1}
+                </span>
+              ))}
+            </span>
+            <button
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
     </div>
